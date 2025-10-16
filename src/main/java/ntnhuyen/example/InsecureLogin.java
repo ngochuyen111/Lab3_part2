a7cac3af47f82bd4ba3d7d95ecc8cc7acf58f550
ngochuyen111 <nguyenthingochuyen11.1cbq@gmail.com>
2025-10-16 14:00:12 +0700

    Initial commit - Lab3 Part 2 with SonarQube analysis

---
+++ src/main/java/ntnhuyen/example/InsecureLogin.java
@@ -0,0 +1,28 @@
+package ntnhuyen.example;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class InsecureLogin {
+
+    private static final Logger logger =
+            Logger.getLogger(InsecureLogin.class.getName());
+
+    public static void login(String username, String password) {
+        if (username.equals("admin") &&
+                password.equals(System.getenv("ADMIN_PASSWORD"))) {
+            logger.info("Login successful");
+        } else {
+            logger.warning("Login failed");
+        }
+    }
+
+    public void printUserInfo(String user) {
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info("User: " + user);
+        }
+
+    }
+
+    // Đã xoá hàm doNothing() vì không dùng
+}
